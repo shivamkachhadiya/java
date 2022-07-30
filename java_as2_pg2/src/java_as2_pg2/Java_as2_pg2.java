1f27e04e9784425f9c2d762962b0a50358ec1eb2
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2022-07-30 20:13:10 +0530

    Add files via upload

---
+++ java_as2_pg2/src/java_as2_pg2/Java_as2_pg2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_as2_pg2;
+import java.sql.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class Java_as2_pg2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+    }
+    
+}
